d118de7d3ee42b0e9e61caf3fdee017384f6c308
furkan.ulutas <furkan.ulutas@next4biz.com>
2024-03-05 17:40:12 +0300

    Work completed

--- app/page.jsx
+++ app/page.jsx
@@ -1,13 +1,15 @@
 import About from "@/components/About";
 import Hero from "@/components/Hero";
 import Services from "@/components/Services";
+import Work from "@/components/Work";
 
 export default function Home() {
   return (
-  <main>
-    <Hero />
-    <About />
-    <Services />
-  </main>
+    <main>
+      <Hero />
+      <About />
+      <Services />
+      <Work />
+    </main>
   );
 }
