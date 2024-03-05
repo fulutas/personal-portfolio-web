a8775fee8a6e6c9faa2a2a094287f8aa3ef9ecc5
furkan.ulutas <furkan.ulutas@next4biz.com>
2024-03-05 15:35:11 +0300

    Services completed

--- components/About.jsx
+++ components/About.jsx
@@ -117,7 +117,7 @@ const About = () => {
 	}
 
 	return (
-		<section className="xl:h-[1000px] pb-12 xl:py-24">
+		<section className="pb-12 xl:py-24">
 			<div className="container mx-auto">
 				<h2 className="section-title mb-8 xl:mb-16 text-center mx-auto">About me</h2>
 				<div className="flex flex-col xl:flex-row">
