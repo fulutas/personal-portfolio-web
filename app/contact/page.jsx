fe5cbef2e89b4af774c8dd7eda7d226753c8218a
Furkan Ulutaş <ulutasfurkan@gmail.com>
2024-03-12 14:47:30 +0300

    contact page completed

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,4 +1,5 @@
 import { MailIcon, HomeIcon, PhoneCall } from 'lucide-react'
+import Form from '@/components/Form'
 
 const Contact = () => {
   return (
@@ -13,11 +14,25 @@ const Contact = () => {
             <h1 className='h1 max-w-md mb-8'>Let's Work Together.</h1>
             <p className='subtitle max-w-[400px]'>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
           </div>
-          <div className='hidden xl:flex w-full bg-contact_illustration_light dark:bg-contact_illustration_dark bg-contain bg-top bg-no-repeat'>
-
-          </div>
+          <div className='hidden xl:flex w-full bg-contact_illustration_light dark:bg-contact_illustration_dark bg-contain bg-top bg-no-repeat'></div>
         </div>
-        <div>info</div>
+        <div className="grid xl:grid-cols-2 mb-24 xl:mb-32">
+          <div className="flex flex-col gap-y-4 xl:gap-y-14 mb-12 xl:mb-24 text-base xl:text-lg">
+            <div className="flex items-center gap-x-8">
+              <MailIcon size={18} className="text-primary" />
+              <div>youremail@gmail.com</div>
+            </div>
+            <div className="flex items-center gap-x-8">
+              <HomeIcon size={18} className="text-primary" />
+              <div>8420 Pine Avenue, Meadowville MC 833</div>
+            </div>
+            <div className="flex items-center gap-x-8">
+              <PhoneCall size={18} className="text-primary" />
+              <div>+44 123 549 869</div>
+            </div>
+          </div>
+          <Form />
+        </div>
       </div>
     </section>
   )
